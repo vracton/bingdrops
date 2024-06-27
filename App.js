9a13dab8c00eab08a2f66bf6ab11fe7fafe7ffc2
vracton <you@example.com>
2024-06-26 23:48:55 -0500

    init

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test App</Text>
+      <Text>1st Version</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff0000',
+    color: "#fff",
     alignItems: 'center',
     justifyContent: 'center',
   },
