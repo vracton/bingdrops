b4c617af374cbbe796eb7578a7c04f94001afc5d
vracton <you@example.com>
2024-06-27 18:35:22 -0500

    settings mockup

--- Settings.js
+++ Settings.js
@@ -1,9 +1,51 @@
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import CheckBox from 'expo-checkbox';
 
 export default function SettingsScreen() {
+  const [isSelected, setSelection] = useState(false);
+  const [darkMode, setSel] = useState(false);
+  const [savePrev, setSele] = useState(false);
   return (
-    <View style={{backgroundColor: "#1C1C1E", flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{color:"white"}}>Settings</Text>
+    <View style={{backgroundColor: "#1C1C1E", flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <View style={styles.checkboxContainer}>
+        <CheckBox
+        value={darkMode}
+        onValueChange={setSel}
+          style={styles.checkbox}
+        />
+        <Text style={styles.label}>Dark Mode</Text>
+      </View>
+      <View style={styles.checkboxContainer}>
+        <CheckBox
+        value={savePrev}
+        onValueChange={setSele}
+          style={styles.checkbox}
+        />
+        <Text style={styles.label}>Automatically Save Previous Images</Text>
+      </View>
+      <View style={styles.checkboxContainer}>
+        <CheckBox
+        value={isSelected}
+          onValueChange={setSelection}
+          style={styles.checkbox}
+        />
+        <Text style={styles.label}>Download in 4k?</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  checkboxContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  checkbox: {
+    alignSelf: 'center',
+  },
+  label: {
+    margin: 8,
+    color: "white"
+  },
+});
